7410b88949456237572b27b77326d21814148e20
fenek <sivitsky@front.ru>
2017-12-09 02:04:48 +0300

    version with three cases

--- coder.rb
+++ coder.rb
@@ -7,16 +7,22 @@ word = gets.strip
 
 puts "Каким способом зашифровать
      1. MD5
-     2. SHA1"
+     2. SHA1
+     3. SHA2"
 
 id = gets.strip.to_i
 
-if id == 1
-  code = Digest::SHA1.hexdigest word
-  puts code
-elsif id == 2
+case id
+when 1
   code = Digest::MD5.hexdigest word
   puts code
+when 2
+  code = Digest::SHA1.hexdigest word
+  puts code
+  when 3
+    code = Digest::SHA2.hexdigest word
+    puts code
+
 else
   puts "Введите метод шифрования"
 end
